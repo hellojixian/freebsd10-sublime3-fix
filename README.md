ed3ba712d47be5173eb75669f9c5e12f93dceee6
Jixian Wang <jixian@shadowgrid.io>
2016-05-13 15:28:52 +0200

    done

--- README.md
+++ README.md
@@ -1,11 +1,15 @@
 Its time to make some open source contribution again. :-)
 
+# Background
 Sublime 3 is still my most favorate script source code editor, Recently I am working on the project that requires to use freebsd as daily development os. and by default if you have installed linux-sublime3 by pkg command. it will not works, and even not show any error messages. 
 
-that is because sublime3 is ported from linux, and by default installation of freebsd is not fully enabled linux compatibile settings.
+that is because sublime3 is ported from linux, and by default installation of freebsd is not fully enabled linux compatibile settings. 
+
+and even linux64.ko has been loaded by default, you will still need linux.ko for 32bit linux ELF compatiblilty
 
 so this script will help you to fix the issue.
 
+# How to use
 Just simple check out the project and run below script, you will see it get fixed and the lovely sublime3 will popup now.
 it also automatic fixed the system configuration. so next time you will not need to run this script again.
 
--- sublime3-fix.sh
+++ sublime3-fix.sh
@@ -1,1 +1,55 @@
 #!/bin/sh
+
+if [ ! `whoami` == "root" ] ; then
+	echo "Need root permission to permanently fix the issue"
+	echo "Please try it with sudo like below:"
+	echo "
+	sudo $0
+		"
+	exit 1;
+fi
+
+#check if linux.ko loaded
+if ! (kldstat -v | grep -q linux.ko); then
+	kldload linux.ko
+fi
+
+#make linux.ko auto loaded in next boot time
+if ! (cat /etc/rc.conf | grep -q linux_enable="YES"); then 
+	echo linux_enable="YES" >> /etc/rc.conf
+fi
+
+#fix devfs
+if ! (cat /etc/devfs.conf | grep -q "link\s*/tmpfs\s*shm"); then
+	printf "link\t/tmpfs\tshm\n" >> /etc/devfs.conf
+	/etc/rc.d/devfs restart
+fi
+
+# try to mount all before test each pseudo fs
+mount -a
+
+#fix proc/ 
+if ! (mount | grep -q "devfs\s*on"); then
+	printf "proc\t\t\t/proc\tprocfs\trw\t0\t0\n" >> /etc/fstab
+fi
+
+if ! (mount | grep -q "fdesc\s*on"); then
+	printf "fdesc\t\t\t/dev/fd\tfdescfs\trw\t0\t0\n" >> /etc/fstab
+fi
+
+
+if ! (mount | grep -q "tmpfs\s*on"); then
+	printf "tmpfs\t\t\t/tmpfs\ttmpfs\trw,mode=777\t0\t0\n" >> /etc/fstab
+fi
+
+# try to mount them all again
+mount -a
+
+# if the main entry point is not exists then need to reinstall it
+if [ ! -f /usr/local/bin/sublime ];
+	pkg update
+	pkg install -f linux-sublime3
+fi
+
+# there you go , fixed 
+/usr/local/bin/sublime
